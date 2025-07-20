2a83a5db03c655dd2d2fe311de3c37e1a6d23cf0
BacharKarhani <132472406+BacharKarhani@users.noreply.github.com>
2025-07-20 14:42:16 +0300

    //

--- app/Http/Controllers/Api/Admin/HomePage/EventScheduleController.php
+++ app/Http/Controllers/Api/Admin/HomePage/EventScheduleController.php
@@ -5,32 +5,47 @@ namespace App\Http\Controllers\Api\Admin\Homepage;
 use App\Http\Controllers\Controller;
 use App\Models\Day;
 use Illuminate\Http\JsonResponse;
+use Carbon\Carbon;
 
 class EventScheduleController extends Controller
 {
     public function index(): JsonResponse
     {
-        $data = Day::with('events')->get()->map(function (Day $day) {
-            return [
-                'day' => $day->day_title,
-                'date' => $day->date ? $day->date->format('d F Y') : null,
-                'events' => $day->events->map(function ($event) {
-                    return [
-                        'title' => $event->title,
-                        'description' => $event->description,
-                        'image' => (!empty($event->image) && file_exists(public_path('images/' . $event->image)))
-                            ? asset('images/' . $event->image)
-                            : asset('images/default-placeholder.jpg'),
-                        'time' => $event->time,
-                        'address' => $event->address,
-                    ];
-                }),
-            ];
-        });
+        try {
+            $days = Day::with('events')->get()->map(function (Day $day) {
+                return [
+                    'id' => $day->id,
+                    'day_title' => $day->day_title,
+                    'date' => $day->date ? Carbon::parse($day->date)->format('d F Y') : null,
+                    'events' => $day->events->map(function ($event) {
+                        return [
+                            'id' => $event->id,
+                            'title' => $event->title,
+                            'description' => $event->description,
+                            'image' => (!empty($event->image) && file_exists(public_path('images/' . $event->image)))
+                                ? asset('images/' . $event->image)
+                                : asset('images/default-placeholder.jpg'),
+                            'time' => $event->time,
+                            'address' => $event->address,
+                        ];
+                    }),
+                ];
+            });
 
-        return response()->json([
-            'success' => true,
-            'data' => $data,
-        ]);
+            return response()->json([
+                'success' => true,
+                'data' => $days,
+            ]);
+        } catch (\Throwable $e) {
+            \Log::error('EventScheduleController@index Error', [
+                'message' => $e->getMessage(),
+                'trace' => $e->getTraceAsString()
+            ]);
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Something went wrong while fetching the event schedule.'
+            ], 500);
+        }
     }
 }
